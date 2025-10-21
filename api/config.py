444dabd5c50bc5cbb8a94ced56adb4a52e97e9de
DESI-TECH <pravins.gpt@gmail.com>
2025-10-22 01:59:19 +0530

    Delete api directory

--- api/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8397950433:AAEGOSrx1_a3XWVMpifv8Z_WFj1oqZ0HRXk")
-BASE_URL = os.environ.get("BASE_URL", "https://your-vercel-domain.vercel.app")  # used to build track links
-WEBHOOK_PATH = "./api/webhook"
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./data.db")
-ADMIN_CHAT_ID = os.environ.get("6206420660")  # optional admin to notify
